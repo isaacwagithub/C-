7cbfa45907fe4a9a845f586fa5ea6a4b572db180
isaacwagithub <imokoena268@gmail.com>
2020-12-01 10:59:34 +0200

    Add files via upload

---
+++ BoxingTutorial19112020/BoxingTutorial19112020/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BoxingTutorial19112020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 3;
+            object o = i;   //boxing
+            Console.WriteLine(o);
+
+            int j = (int)o; //unboxing
+            Console.WriteLine(i);
+        }
+    }
+}
