6d0ab6ea7618594315d2394db8ddb1f81059dca2
isaacwagithub <imokoena268@gmail.com>
2020-12-01 10:55:40 +0200

    Add files via upload

---
+++ AddTwoNumbers/AddTwoNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AddTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            int number1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number");
+            int number2 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine($" {number1} + {number2} = {number1 + number2}");
+        }
+    }
+}
