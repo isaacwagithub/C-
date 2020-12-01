a8ad439e495acbaa352c9412442f9e81f462972a
isaacwagithub <imokoena268@gmail.com>
2020-12-01 11:52:08 +0200

    Add files via upload

---
+++ GenericsTutorial19112020/GenericsTutorial19112020/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsTutorial19112020
+{
+    class MyGenericArray<T>
+    {
+        private T[] array;
+
+        public MyGenericArray(int size) 
+        {
+            //array = new T(size + 1);
+        }
+    }
+    class Program
+    {
+        static void Swap<T>(ref T lhs, ref T rhs) 
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+        static void Main(string[] args)
+        {
+            int a, b;
+            char c, d;
+            a = 10;
+            b = 20;
+            c = 'I';
+            d = 'Y';
+
+            //display values before swap
+            Console.WriteLine("Int values before calling swap");
+            Console.WriteLine("a = {0} and b = {1}", a,b);
+            Console.WriteLine("Char values before calling swap");
+            Console.WriteLine("c = {0} and d = {1}", c,d);
+
+            //Call swap
+            Swap<int>(ref a, ref b);
+            Swap<char>(ref c, ref d);
+            Console.WriteLine("Int values after calling swap");
+            Console.WriteLine("a = {0} and b = {1}", a, b);
+            Console.WriteLine("Char values after calling swap");
+            Console.WriteLine("c = {0} and d = {1}", c, d);
+
+
+        }
+    }
+}
